7f114b552d3a3005afd065d0dfb4e03063badb2a
ThomasF85 <thomas.foeldi@gmail.com>
2023-12-03 14:18:20 +0100

    fix backup route

--- src/app/api/backup/route.ts
+++ src/app/api/backup/route.ts
@@ -1,8 +1,12 @@
 import { ServerVocab } from "@/server/ServerVocab";
 import { getAll } from "@/server/db/mongodbService";
 import fs from "fs";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const allVocabs: ServerVocab[] = await getAll();
   fs.writeFileSync("vocabs.json", JSON.stringify(allVocabs, null, 2));
+  return NextResponse.json({
+    message: `Wrote vocabs to file`,
+  });
 }
